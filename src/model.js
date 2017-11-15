ecfb41b80a5dba167d5f673809aea3f3cc15bc83
Kay Ueda <Lizzy@Kays-MacBook-Pro.local>
2017-11-15 15:07:42 -0800

    l

---
+++ src/model.js
@@ -0,0 +1,84 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Bear = require('./BearModel.js');
+
+const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
+const server = express();
+
+// allow server to parse JSON bodies from POST/PUT/DELETE requests
+server.use(bodyParser.json());
+
+//=======================================
+
+const BearSchema = new mongoose.Schema({
+    species: {
+        type: String,
+        required: true
+    },
+    latinName: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+// TODO: write your server code here
+server.post('/api/bears', function(req, res) {
+  const newBear = new Bear(req.body);
+  //check the bear has all the data
+  newBear.save(function(err, bear) {
+    if(err) {
+      res.status(STATUS_SERVER_ERROR).json({ error: "Could not create bear."})
+    } else {
+      res.status(201).json(bear);
+    }
+  });
+});
+
+server.get('/api/bears', function(req, res) {
+  Bear.find({}, function(err, bears) {
+    if(err) {
+      //handle error
+    } else {
+      res.status(200).json(bears);
+    }
+  })
+});
+
+server.get('/api/bears/:id', function(req, res) {
+  const { id } = req.params;
+
+  Bear.findById({ _id: id }, function(err, bears) {
+    if(err) {
+      //handle error
+    } else {
+      res.status(200).json(bears);
+    }
+  });
+})
+
+
+//plumbings
+mongoose.Promise = global.Promise;
+const connect = mongoose.connect(
+  'mongodb://localhost/bears',
+  { useMongoClient: true }
+);
+
+/* eslint no-console: 0 */
+connect.then(() => {
+  const port = 3000;
+  server.listen(port);
+  console.log(`Server Listening on ${port}`);
+}, (err) => {
+  console.log('\n************************');
+  console.log("ERROR: Couldn't connect to MongoDB. Do you have it running?");
+  console.log('************************\n');
+});
